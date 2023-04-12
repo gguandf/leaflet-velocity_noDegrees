13a95c69762ef713077f15c465558bb0ccee6b9e
gguandf <gunter.gebetsberger@gmail.com>
2023-04-12 14:45:45 +0200

    Update L.Control.Velocity.js
    remove Degrees

--- src/js/L.Control.Velocity.js
+++ src/js/L.Control.Velocity.js
@@ -148,11 +148,11 @@ L.Control.Velocity = L.Control.extend({
       gridValue[2]
     ) {
     var deg = self.vectorToDegrees(gridValue[0], gridValue[1], this.options.angleConvention);
-    var cardinal = this.options.showCardinal ? ` (${self.degreesToCardinalDirection(deg)}) ` : '';
+    var cardinal = this.options.showCardinal ? ` ${self.degreesToCardinalDirection(deg)} ` : '';
 
 		htmlOut = `<strong> ${this.options.velocityType} ${
 			this.options.directionString
-		}: </strong> ${deg.toFixed(2)}°${cardinal}, <strong> ${this.options.velocityType} ${
+		}: </strong> ${cardinal}, <strong> ${this.options.velocityType} ${
 			this.options.speedString
 		}: </strong> ${self
 			.vectorToSpeed(gridValue[0], gridValue[1], this.options.speedUnit)
